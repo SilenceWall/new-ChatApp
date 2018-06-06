4b877e485ca667715444380bd1d78159c5f69a4c
Anas Shahin <anasshahin@Anass-MacBook-Pro.local>
2018-06-06 05:25:09 +0300

    brodcast a mesage

--- server/server.js
+++ server/server.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const path = require('path');
-
+const socketIO = require('socket.io');
+const http = require('http');
 var port = process.env.PORT || 9999;
 var app = express();
 var myPath = path.join(__dirname, '../public');
+
 app.use(express.static(myPath));
 
 app.get('/', (req, res)=>{
   res.send('hellow');
 });
+var server = http.createServer(app);
+var io = socketIO(server);
+io.on('connection', (socket) => {
+  console.log('connection established');
 
-app.listen(port, ()=>{
+
+  socket.on('createMessage', (message)=>{
+    io.emit('newMessage', {
+      message: message.message,
+      sender: message.sender
+    });
+  });
+  socket.on('disconnect', ()=>{
+    console.log('connection disconnected');
+  });
+});
+
+
+
+
+server.listen(port, ()=>{
   console.log('Ok, Server is ready');
 });
